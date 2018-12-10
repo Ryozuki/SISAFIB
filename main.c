24a6490bd1bb573f66c72972a6968e95e991846f
Ryozuki <649504-Ryozuki@users.noreply.gitlab.com>
2018-12-10 17:20:56 +0100

    improvements2

--- main.c
+++ main.c
@@ -7,8 +7,23 @@
 #define TOKEN_SPLIT " ,\n"
 #define MEMORY_SIZE 65536 // 2^16
 
+#define FFF_AND 0
+#define FFF_OR 1
+#define FFF_XOR 2
+#define FFF_NOT 3
+#define FFF_ADD 4
+#define FFF_SUB 5
+#define FFF_SHA 6
+#define FFF_SHL 7
+
+#define FFF_CMPLT 0
+#define FFF_CMPLE 1
+#define FFF_CMPEQ 3
+#define FFF_CMPLTU 4
+#define FFF_CMPLEU 5
+
 static size_t current_line = 1;
-static size_t current_address = 0;
+static size_t PC = 0;
 
 int16_t int_pow(int16_t x, int16_t y) {
 	while(y--)
@@ -24,7 +39,7 @@ uint16_t uint_pow(uint16_t x, uint16_t y) {
 
 void print_error(const char* format, ...) {
 	va_list args;
-	printf("Error parsing (line %llu): ", current_line);
+	printf("Error parsing (line %lu): ", current_line);
 	va_start(args, format);
 	vprintf(format, args);
 	va_end(args);
@@ -32,7 +47,7 @@ void print_error(const char* format, ...) {
 
 void print_warning(const char* format, ...) {
 	va_list args;
-	printf("Warning (line %llu): ", current_line);
+	printf("Warning (line %lu): ", current_line);
 	va_start(args, format);
 	vprintf(format, args);
 	va_end(args);
@@ -61,8 +76,8 @@ int get_register(char** token, size_t current_line) {
 	return 1;
 }
 
-int get_register_num(char* token) {
-	return strtol(&token[1], NULL, 10);
+int16_t get_register_num(char* token) {
+	return (int16_t)strtol(&token[1], NULL, 10);
 }
 
 int get_3reg(char** token, int* reg_d, int* reg_a, int* reg_b) {
@@ -120,6 +135,26 @@ void printf_bits(const char *format, int num, int bits) {
 	}
 }
 
+int16_t format_in_out(int16_t reg, int8_t addr, int16_t is_out) {
+	// OP | REG_DEST | IN/OUT | ADDR
+	return (int16_t) ((10 << 12) | (reg << 9) | (is_out << 8) | addr);
+}
+
+int16_t format_mov(int16_t reg, int8_t value, int16_t is_movhi) {
+	return (int16_t) ((9 << 12) | (reg << 9) | (is_movhi << 8) | value);
+}
+
+
+int16_t format_r3(int16_t dreg, int16_t areg, int16_t breg, int16_t f, int16_t is_boolean) {
+	return (int16_t) ((is_boolean << 12) | (areg << 9) | (breg << 6) | (dreg << 3) | f);
+}
+
+void printf_info(int reg, int16_t cur_reg_val, int16_t old_reg_val, int16_t formatted, const char* format) {
+	printf("R%d (0x%04X -> 0x%04X): ", reg , old_reg_val & 0xffff, cur_reg_val & 0xffff);
+	printf_bits(format, formatted, 16);
+	printf(" (%04X)\n", formatted & 0xffff);
+}
+
 int main(int argc, const char *argv[]) {
 	int16_t Registers[8] = {0};
 	int8_t Memory[MEMORY_SIZE] = {0};
@@ -157,7 +192,9 @@ int main(int argc, const char *argv[]) {
 			if(!get_register(&token, current_line))
 				break;
 
-			const int reg = get_register_num(token);
+			const int16_t reg = get_register_num(token);
+			const int16_t old_reg_val = Registers[reg];
+			// TODO: Make a system to have input and output?
 
 			token = strtok(NULL, TOKEN_SPLIT);
 
@@ -167,10 +204,27 @@ int main(int argc, const char *argv[]) {
 			}
 
 			const int8_t addr = (int8_t)strtol(token, NULL, 10);
-			const int16_t formatted = (10 << 12) | (reg << 9) | addr;
+			const int16_t formatted = format_in_out(reg, addr, 0);
 
-			printf_bits("xxxx xxx x xxxxxxxx\n", formatted, 16);
-			printf("%X", formatted);
+			printf_info(reg, Registers[reg], old_reg_val, formatted, "xxxx xxx x xxxxxxxx");
+		}
+		else if (strcmp(token, "OUT") == 0) {
+			token = strtok(NULL, TOKEN_SPLIT);
+			if (token == NULL) {
+				print_error("found 'OUT' token without a address.\n");
+				break;
+			}
+			const int8_t addr = (int8_t)strtol(token, NULL, 10);
+
+			if(!get_register(&token, current_line))
+				break;
+
+			const int16_t reg = get_register_num(token);
+			const int16_t old_reg_val = Registers[reg];
+
+			const int16_t formatted = format_in_out(reg, addr, 1);
+
+			printf_info(reg, Registers[reg], old_reg_val, formatted, "xxxx xxx x xxxxxxxx");
 		}
 		else if(!strcmp(token, "MOVI")) {
 			if(!get_register(&token, current_line))
@@ -191,8 +245,10 @@ int main(int argc, const char *argv[]) {
 				print_warning("Value passed to MOVI (%d) uses more capacity than the allowed for a 8 bit signed integer.\n", val);
 
 			int8_t value = (int8_t)val;
+			int16_t old_value = Registers[reg];
 			Registers[reg] = value;
-			printf("R%d: 0x%04X\n", reg, Registers[reg]);
+			int16_t formatted = format_mov(reg, value, 0);
+			printf_info(reg, Registers[reg], old_value, formatted, "xxxx xxx x xxxxxxxx");
 		}
 		else if(!strcmp(token, "MOVHI")) {
 			if(!get_register(&token, current_line))
@@ -215,78 +271,118 @@ int main(int argc, const char *argv[]) {
 			int8_t value = (int8_t)val;
 
 			// 0xff cleans the upper bits.
+			int16_t old_value = Registers[reg];
 			Registers[reg] = (Registers[reg] & 0xFF) | (value << 8);
-			printf("R%d: 0x%04X\n", reg, Registers[reg]);
+			int16_t formatted = format_mov(reg, value, 1);
+			printf_info(reg, Registers[reg], old_value, formatted, "xxxx xxx x xxxxxxxx");
 		}
 		else if(!strcmp(token, "ADD")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = Registers[a] + Registers[b];
-			printf("R%d: 0x%04X\n", d, Registers[d]);
+			int16_t formatted = format_r3(d, a, b, FFF_ADD, 0);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "SUB")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = Registers[a] - Registers[b];
+			int16_t formatted = format_r3(d, a, b, FFF_SUB, 0);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "AND")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = Registers[a] & Registers[b];
+			int16_t formatted = format_r3(d, a, b, FFF_AND, 0);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "OR")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = Registers[a] | Registers[b];
+			int16_t formatted = format_r3(d, a, b, FFF_OR, 0);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "XOR")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = Registers[a] ^ Registers[b];
+			int16_t formatted = format_r3(d, a, b, FFF_XOR, 0);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "NOT")) {
 			int d, a;
+			int16_t old_value = Registers[d];
 			get_2reg(&token, &d, &a);
 			Registers[d] = ~Registers[a];
+			int16_t formatted = format_r3(d, a, 0, FFF_NOT, 0);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "SHA")) {
 			int d, a, b;
+			int16_t old_value = Registers[d];
 			get_3reg(&token, &d, &a, &b);
 			Registers[d] = Registers[a] * int_pow(2, (Registers[b] & 8) | (Registers[b] & 4) | (Registers[b] & 2) | (Registers[b] & 1));
+			int16_t formatted = format_r3(d, a, b, FFF_SHA, 0);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "SHL")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			uint16_t regb = (uint16_t)Registers[b];
 			Registers[d] = (uint16_t)Registers[a] * uint_pow(2, (regb & 8) | (regb & 4) | (regb & 2) | (regb & 1));
+			int16_t formatted = format_r3(d, a, b, FFF_SHL, 0);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "CMPLT")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = Registers[a] < Registers[b];
+			int16_t formatted = format_r3(d, a, b, FFF_CMPLT, 1);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "CMPLE")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = Registers[a] <= Registers[b];
+			int16_t formatted = format_r3(d, a, b, FFF_CMPLE, 1);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "CMPEQ")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = Registers[a] == Registers[b];
+			int16_t formatted = format_r3(d, a, b, FFF_CMPEQ, 1);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "CMPLTU")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = (uint16_t)Registers[a] < (uint16_t)Registers[b];
+			int16_t formatted = format_r3(d, a, b, FFF_CMPLTU, 1);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 		else if(!strcmp(token, "CMPLEU")) {
 			int d, a, b;
 			get_3reg(&token, &d, &a, &b);
+			int16_t old_value = Registers[d];
 			Registers[d] = (uint16_t)Registers[a] <= (uint16_t)Registers[b];
+			int16_t formatted = format_r3(d, a, b, FFF_CMPLEU, 1);
+			printf_info(d, Registers[d], old_value, formatted, "XXXX XXX XXX XXX XXX");
 		}
 
-		++current_address;
+		++PC;
 		++current_line;
 	}
 
